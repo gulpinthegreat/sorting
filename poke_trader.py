238a454c1cd155ce758e757d0f8681e2677ef984
gulpinthegreat <90713423+gulpinthegreat@users.noreply.github.com>
2022-01-13 10:24:07 -0500

    Create poke_trader.py

---
+++ poke_trader.py
@@ -0,0 +1,18 @@
+poke_party = ["PIKACHU", "CHARMELEON", "GEODUDE", "GYARADOS", "BUTTERFREE", "MANKEY"]
+
+while True:
+    print("EXCHANGE POKEMON\n")
+    print("0. " + poke_party[0])
+    for i in range(1, len(poke_party)):
+        print(f"\t{i}. {poke_party[i]}")
+
+    print(f"\nChoose a Pokemon to exchange with {poke_party[0]}. (Or enter 0 to quit.)")
+    x = int(input("> "))
+
+    # add code here to swap the Pokemon in index 0 with the Pokemon at index x
+    pokemon = poke_party[0]
+    poke_party[0] = poke_party[x]
+    poke_party[x] = pokemon
+
+    if x == 0:
+        break
